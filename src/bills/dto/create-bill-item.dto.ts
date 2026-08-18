393df5c7b60a2b5855f5bbe502e749fbd6bf671c
eliandev <jevillaurrutia@gmail.com>
2026-08-18 02:25:06 -0500

    add doc by swagger

--- src/bills/dto/create-bill-item.dto.ts
+++ src/bills/dto/create-bill-item.dto.ts
@@ -1,9 +1,12 @@
 import { IsInt, IsUUID, Min } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateBillItemDto {
+  @ApiProperty({ format: 'uuid' })
   @IsUUID()
   productId!: string;
 
+  @ApiProperty({ example: 2, minimum: 1 })
   @IsInt()
   @Min(1)
   quantity!: number;
