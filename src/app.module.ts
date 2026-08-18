181dfba6f1e98fd58c9c6fc25411c20dd197ed31
eliandev <jevillaurrutia@gmail.com>
2026-08-18 00:58:52 -0500

    add connection to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, type TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService): TypeOrmModuleOptions => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST', 'localhost'),
+        port: Number(configService.get<string>('DB_PORT', '5432')),
+        username: configService.get<string>('DB_USERNAME', 'admin'),
+        password: configService.get<string>('DB_PASSWORD', 'password123'),
+        database: configService.get<string>('DB_NAME', 'ferret-db'),
+        autoLoadEntities: true,
+        synchronize: false,
+        retryAttempts: 10,
+        retryDelay: 3000,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
